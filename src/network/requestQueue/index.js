d220eb25fa193f707a9df82cb3e8504eb23d5795
Robert Shaw <rshaw@foundationfinance.com>
2024-07-03 13:31:51 -0500

    fix checking for pending requests in an infinite loop

--- src/network/requestQueue/index.js
+++ src/network/requestQueue/index.js
@@ -294,7 +294,9 @@ module.exports = class RequestQueue extends EventEmitter {
       this[PRIVATE.EMIT_QUEUE_SIZE_EVENT]()
     }
 
-    this.scheduleCheckPendingRequests()
+    if (this.pending.length) {
+      this.scheduleCheckPendingRequests()
+    }
   }
 
   /**
